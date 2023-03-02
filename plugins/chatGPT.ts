9d9522a6bd5ce9c49fdf6f80d0969ffc09dd44f7
Misaka19614 <inzamzheng@icloud.com>
2023-03-02 20:03:55 +0800

    fix: fix the request code

--- plugins/chatGPT.ts
+++ plugins/chatGPT.ts
@@ -1,40 +1,45 @@
 import { Message, log } from "wechaty";
-import { Config } from "../config";
 import https from 'https';
 
-var bot_config = new Config();
-var openai_api_key = process.env.OPEN_API_KEY;
+var openai_api_key = process.env.OPENAI_API_KEY;
 
 export async function chatGPT(message: Message) {
-    if (await message.mentionSelf())
-    {
-        let url = "https://api.openai.com/v1/chat/completions"
-        let headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + openai_api_key
-        }
-        let data = {
-            "model": "gpt-3.5-turbo",
-            "message": [
-                {
-                    "role": "user",
-                    "content": message.text()
-                }
-            ]
-        }
-        let options = {
-            method: "POST",
-            headers: headers,
-            body: JSON.stringify(data)
-        };
-        https.get(url, options, (res: any) => {
-            res.on('data', (chat) => {
-                chat = JSON.parse(chat);
-                message.say(chat.data.choices[0].message[0].content);
-                log.silly("[Chat GPT]", `send message: ${chat.data.choices[0].text}`);
+
+    if (await message.mentionSelf()) {
+        try {
+            let url = "https://api.openai.com/v1/chat/completions"
+            let headers = {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + openai_api_key
+            }
+            let data = {
+                "model": "gpt-3.5-turbo",
+                "message": [
+                    {
+                        "role": "user",
+                        "content": message.text().substring(message.text().indexOf(" ") + 1)
+                    }
+                ]
+            }
+            let options = {
+                method: "POST",
+                headers: headers,
+                body: data
+            };
+            log.info("[Chat GPT]", `asked: ${message.text().substring(message.text().indexOf(" ") + 1)}`);
+            https.request(url, options, (res: any) => {
+                res.on('data', (chat) => {
+                    chat = JSON.parse(chat);
+                    console.log(chat);
+                    message.say(chat.choices[0].message[0].content);
+                    log.silly("[Chat GPT]", `send message: ${chat.data.choices[0].text}`);
+                });
+            }).on('error', (e: any) => {
+                console.error(e);
             });
-        }).on('error', (e: any) => {
+        }
+        catch (e) {
             console.error(e);
-        });
+        }
     }
 }
